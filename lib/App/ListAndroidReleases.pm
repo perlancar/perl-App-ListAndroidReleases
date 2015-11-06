0edc647ce2d86237a65a641c900eb6046bb07b01
perlancar (@pc-office) <perlancar@gmail.com>
2015-11-06 12:07:21 +0700

    Initial commit

---
+++ lib/App/ListAndroidReleases.pm
@@ -0,0 +1,13 @@
+package App::ListAndroidReleases;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List Android releases (CLI)
+
+=head1 DESCRIPTION
+
+See the included script L<list-android-releases>.
+
+=cut
